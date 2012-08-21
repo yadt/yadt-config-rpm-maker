d481c34390bbf4973eae6232965a494d43338016
sherold <sebastian.herold@immobilienscout24.de>
2012-08-21 16:04:18 +0200

    load expected rpms from config

--- test/configRpmMaker.py
+++ test/configRpmMaker.py
@@ -32,20 +32,11 @@ class ConfigRpmMakerTest(unittest.TestCase):
         config_rpm_maker = ConfigRpmMaker(test_config.get('svn_build_revision'), svn_service)
 
         rpms = config_rpm_maker.build()
-        self.assertEqual(int(test_config.config.get('svn_build_rpm_count')), len(rpms))
-        self.assertRpm('devweb01', rpms)
-        self.assertRpm('tuvweb01', rpms)
-        self.assertRpm('berweb01', rpms,
-            requires = ['all-req', 'all-req2', 'ber-req', 'ber-req2', 'pro-req'],
-            provides = ['all-prov', 'all-prov2', 'all-prov3', 'pro-prov', 'pro-prov2'],
-            files={
-                'files/file_from_all' : '',
-                'files/file_from_ber' : '',
-                'files/file_from_pro' : '',
-                'files/override' : 'berweb',
-                'vars/override' : 'berweb',
-                'vars/var_in_var' : 'berwebberweb',
-                })
+        self.assertEqual(int(test_config.get('svn_build_rpm_count')), len(rpms))
+
+        hosts_to_check = test_config.get('svn_build_hosts')
+        for host in hosts_to_check:
+            self.assertRpm(host, rpms, requires=host.get('requires', None), provides=host.get('provides', None), files=host.get('files', None))
 
     def _cleanup_temp_dir(self):
         temp_dir = config_dev.get('temp_dir')
