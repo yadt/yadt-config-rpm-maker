4910047be163004c6fc4ef6995fcf2aceeb11f69
sherold <sebastian.herold@immobilienscout24.de>
2012-08-21 15:17:04 +0200

    fix syntax error on python 2.6

--- test/configRpmMaker.py
+++ test/configRpmMaker.py
@@ -20,11 +20,11 @@ class ConfigRpmMakerTest(unittest.TestCase):
 
     def test_affected_hosts(self):
         config_rpm_maker = ConfigRpmMaker(None, None)
-        self.assertEqual({'berweb01', 'devweb01', 'tuvweb02'}, config_rpm_maker._get_affected_hosts(['typ/web', 'foo/bar'], ['berweb01', 'devweb01', 'tuvweb02']))
-        self.assertEqual({'berweb01', 'devweb01', 'tuvweb02'}, config_rpm_maker._get_affected_hosts(['foo/bar', 'all'], ['berweb01', 'devweb01', 'tuvweb02']))
-        self.assertEqual({'berweb01', 'tuvweb02'}, config_rpm_maker._get_affected_hosts(['loc/pro', 'loc/tuv'], ['berweb01', 'devweb01', 'tuvweb02']))
-        self.assertEqual({'devweb01'}, config_rpm_maker._get_affected_hosts(['foo/bar', 'loctyp/devweb'], ['berweb01', 'devweb01', 'tuvweb02']))
-        self.assertEqual({'devweb01'}, config_rpm_maker._get_affected_hosts(['foo/bar', 'host/devweb01'], ['berweb01', 'devweb01', 'tuvweb02']))
+        self.assertEqual(set(['berweb01', 'devweb01', 'tuvweb02']), config_rpm_maker._get_affected_hosts(['typ/web', 'foo/bar'], ['berweb01', 'devweb01', 'tuvweb02']))
+        self.assertEqual(set(['berweb01', 'devweb01', 'tuvweb02']), config_rpm_maker._get_affected_hosts(['foo/bar', 'all'], ['berweb01', 'devweb01', 'tuvweb02']))
+        self.assertEqual(set(['berweb01', 'tuvweb02']), config_rpm_maker._get_affected_hosts(['loc/pro', 'loc/tuv'], ['berweb01', 'devweb01', 'tuvweb02']))
+        self.assertEqual(set(['devweb01']), config_rpm_maker._get_affected_hosts(['foo/bar', 'loctyp/devweb'], ['berweb01', 'devweb01', 'tuvweb02']))
+        self.assertEqual(set(['devweb01']), config_rpm_maker._get_affected_hosts(['foo/bar', 'host/devweb01'], ['berweb01', 'devweb01', 'tuvweb02']))
 
     def test_build_hosts(self):
         self._cleanup_temp_dir()
