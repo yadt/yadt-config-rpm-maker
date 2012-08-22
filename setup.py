38a19a5e7dd13943205bde8f7bb5d82ce273ad3c
sherold <sebastian.herold@immobilienscout24.de>
2012-08-22 16:01:32 +0200

    collect correct packages and requires for bdist_rpm

--- setup.py
+++ setup.py
@@ -4,16 +4,25 @@ import os
 from setuptools import setup, find_packages
 import sys
 
-sys.path.append('src')
-
 os.environ['TEST_CONFIG_FILE'] = 'test_config.yaml'
 os.environ['YADT_CONFIG_RPM_MAKER_CONFIG_FILE'] = 'dev_config.yaml'
+
+SRC_DIR = 'src'
+
+sys.path.append(SRC_DIR)
 
 setup(
     name = "yadt-config-rpm-maker",
     version = "2.0",
     license = "GPL",
-    url = "http://code.google.com/p/yadt",
-    packages = find_packages(),
+    url = "https://github.com/yadt/yadt-config-rpm-maker",
+    packages=find_packages(where=SRC_DIR),
+    package_dir = {'': SRC_DIR},
     test_suite = 'test',
+
+    author = "Sebastian Herold",
+    author_email = "sebastian.herold@immobilienscout24.de",
+
+    description = "This program is called as a commit hook in a config SVN repository and automatically creates the necessary RPMs after every commit and puts them in the configured RPM repository.",
+    keywords= "rpm config host svn hook"
 )
