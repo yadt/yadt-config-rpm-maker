c43132ff1f0d09bfeccbfb1b689096086fe0c9ca
sherold <sebastian.herold@immobilienscout24.de>
2012-08-22 12:36:04 +0200

    introduce default config path

--- src/config_rpm_maker/config.py
+++ src/config_rpm_maker/config.py
@@ -4,14 +4,12 @@ import yaml
 
 config = {}
 
-if os.environ.has_key('YADT_CONFIG_RPM_MAKER_CONFIG_FILE') and os.path.exists(os.environ['YADT_CONFIG_RPM_MAKER_CONFIG_FILE']):
-    path = os.environ['YADT_CONFIG_RPM_MAKER_CONFIG_FILE']
-    f = open(path)
-    try:
+config_file_path = os.environ.get('YADT_CONFIG_RPM_MAKER_CONFIG_FILE', '/etc/yadt-config-rpm-maker.yaml')
+if os.path.exists(config_file_path):
+    with open(config_file_path) as f:
         config = yaml.load(f)
-        logging.info("Loaded config file '%s' config: %s", path, str(config))
-    finally:
-        f.close()
+
+    logging.info("Loaded config file '%s' config: %s", config_file_path, str(config))
 
 def get(name, default = None):
     if config.has_key(name):
