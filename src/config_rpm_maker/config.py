d601db52aaeb1956a03880a678c550ec5b2de853
sherold <sebastian.herold@immobilienscout24.de>
2012-08-23 13:36:59 +0200

    make config file mandatory

--- src/config_rpm_maker/config.py
+++ src/config_rpm_maker/config.py
@@ -4,12 +4,17 @@ import yaml
 
 config = {}
 
-config_file_path = os.environ.get('YADT_CONFIG_RPM_MAKER_CONFIG_FILE', '/etc/yadt-config-rpm-maker.yaml')
+config_file_path = os.environ.get('YADT_CONFIG_RPM_MAKER_CONFIG_FILE', 'yadt-config-rpm-maker.yaml')
 if os.path.exists(config_file_path):
-    with open(config_file_path) as f:
-        config = yaml.load(f)
+    try:
+        with open(config_file_path) as f:
+            config = yaml.load(f)
 
-    logging.info("Loaded config file '%s' config: %s", config_file_path, str(config))
+        logging.info("Loaded config file '%s' config: %s", config_file_path, str(config))
+    except Exception as e:
+        raise Exception("Could not load config file '%s'" % config_file_path, e)
+else:
+    raise Exception("Could not find config file '%s'. Please provide a 'yadt-config-rpm-maker.yaml' in the current working directory or set environment variable 'YADT_CONFIG_RPM_MAKER_CONFIG_FILE'." % config_file_path )
 
 def get(name, default = None):
     if config.has_key(name):
