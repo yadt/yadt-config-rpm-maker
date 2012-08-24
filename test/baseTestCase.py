8827e3daa39b4d1326dcc2d3791c7b0d4b9ae28b
sherold <sebastian.herold@immobilienscout24.de>
2012-08-24 16:26:35 +0200

    create temp dir for tests

--- test/baseTestCase.py
+++ test/baseTestCase.py
@@ -10,6 +10,7 @@ class SvnTestCase(unittest.TestCase):
         self.repo_dir = os.path.abspath(os.path.join(config.get('temp_dir'), 'svn_repo'))
         if os.path.exists(self.repo_dir):
             shutil.rmtree(self.repo_dir)
+        os.makedirs(self.repo_dir)
         if subprocess.call('svnadmin create %s' % self.repo_dir, shell=True):
             raise Exception('Could not create svn repo.')
         self.repo_url = 'file://%s' % self.repo_dir
