842ec246b9d92fdba218d13bcef6a4e5e23020ef
Maximilien Riehl <maximilien.riehl@gmail.com>
2012-09-10 09:37:45 +0200

    also unit test for more complex cycle

--- test/cycle.py
+++ test/cycle.py
@@ -16,7 +16,6 @@ class CycleTest (unittest.TestCase):
       graphTest = TokenCycleChecking(graph_with_cycle)
       self.assertRaises(ContainsCyclesException,graphTest.assert_no_cycles_present)
 
-
     def test_does_not_fire_exception_if_graph_empty (self):
       empty_graph={}
       graphTest = TokenCycleChecking(empty_graph)
@@ -30,3 +29,17 @@ class CycleTest (unittest.TestCase):
       graph_with_no_cycles['hello']=['foo']
       graphTest = TokenCycleChecking(graph_with_no_cycles)
       graphTest.assert_no_cycles_present()
+      
+
+    def test_recognizes_complex_cycle(self):
+      graph_with_cycle={}
+      graph_with_cycle['foo']=['bar']
+      graph_with_cycle['bar']=['baz']
+      graph_with_cycle['baz']=['abc']
+      graph_with_cycle['abc']=['foo']
+      graph_with_cycle['f']=['g']
+      graph_with_cycle['j']=['k']
+      graphTest = TokenCycleChecking(graph_with_cycle)
+      self.assertRaises(ContainsCyclesException,graphTest.assert_no_cycles_present)
+
+
