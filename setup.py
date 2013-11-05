54e925e567c3d3e0dd752daa5146a6eaf88731cd
aelgru <aelgru@gmail.com>
2013-11-05 23:34:50 +0100

    ... a bit of sorting ...

--- setup.py
+++ setup.py
@@ -10,20 +10,18 @@ setup(name="yadt-config-rpm-maker",
       version="2.0",
       license="GPL",
       url="https://github.com/yadt/yadt-config-rpm-maker",
-      packages=find_packages(where=SRC_DIR),
-      package_dir={'': SRC_DIR},
-      test_suite='tests',
-      
       author="Sebastian Herold",
       author_email="sebastian.herold@immobilienscout24.de",
-      
       description="This program is called as a commit hook in a config SVN repository and automatically creates the necessary RPMs after every commit and puts them in the configured RPM repository.",
       keywords="rpm config host svn hook",
-      
+
       entry_points={
           'console_scripts': ['config-rpm-maker = config_rpm_maker:mainMethod'],
       },
       setup_requires=[
           "flake8"
-      ]
+      ],
+      packages=find_packages(where=SRC_DIR),
+      package_dir={'': SRC_DIR},
+      test_suite='tests'
 )
