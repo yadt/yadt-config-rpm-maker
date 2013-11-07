c43a65d923700fbf498f2bc86aabecaf4f3fa884
aelgru <aelgru@gmail.com>
2013-11-07 15:50:55 +0100

    removing exception which is thrown and catched in the same function

--- src/config_rpm_maker/__init__.py
+++ src/config_rpm_maker/__init__.py
@@ -28,30 +28,29 @@ from config_rpm_maker import config
 LOGGER = getLogger()
 
 
-class CliException(BaseConfigRpmMakerException):
-    error_info = "Command Line Error:\n"
-
-
 def main(args=sys.argv[1:]):
 
+    if len(args) != 2:
+        LOGGER.error("You need to provide 2 parameters (repo dir, revision). Arguments were %s " % str(args))
+        sys.exit(1)
+
+    if not (args[1].isdigit() and int(args[1]) >= 0):
+        LOGGER.error("Revision must be a positive integer.\nGiven revision was '%s'" % args[1])
+        sys.exit(1)
+
     try:
-        if len(args) != 2:
-            raise CliException("You need to provide 2 parameters (repo dir, revision).\nArguments were %s " % str(args))
-
-        if not (args[1].isdigit() and int(args[1]) >= 0):
-            raise CliException("Revision must be a positive integer.\nGiven revision was '%s'" % args[1])
-
         # first use case is post-commit hook. repo dir can be used as file:/// SVN URL
         svn_service = SvnService(
             base_url='file://' + args[0],
             path_to_config=config.get('svn_path_to_config')
         )
         ConfigRpmMaker(revision=args[1], svn_service=svn_service).build()
+
     except BaseConfigRpmMakerException as e:
         for line in str(e).split("\n"):
             LOGGER.error(line)
-
         sys.exit(1)
+
     except Exception:
         traceback.print_exc(5)
         sys.exit(2)
