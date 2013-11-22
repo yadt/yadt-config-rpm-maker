f4c9fd53b83c8f4796e4e2c5f1379c42e4334ef0
aelgru <aelgru@gmail.com>
2013-11-22 18:30:57 +0100

    Using dictionary literals.

--- test/unittests/token/cycle_test.py
+++ test/unittests/token/cycle_test.py
@@ -24,34 +24,39 @@ from config_rpm_maker.token.cycle import TokenCycleChecking
 
 class CycleTest(unittest.TestCase):
 
-    def test_recognizes_simple_cycle(self):
-        graph_with_cycle = {}
-        graph_with_cycle['foo'] = ['bar']
-        graph_with_cycle['bar'] = ['foo']
-        graphTest = TokenCycleChecking(graph_with_cycle)
-        self.assertRaises(ContainsCyclesException, graphTest.assert_no_cycles_present)
+    def test_should_recognize_simple_cycle(self):
+        graph_with_cycle = {'foo': ['bar'],
+                            'bar': ['foo']}
 
-    def test_does_not_fire_exception_if_graph_empty(self):
+        actual_graph = TokenCycleChecking(graph_with_cycle)
+
+        self.assertRaises(ContainsCyclesException, actual_graph.assert_no_cycles_present)
+
+    def test_should_not_raise_exception_if_graph_empty(self):
         empty_graph = {}
-        graphTest = TokenCycleChecking(empty_graph)
-        graphTest.assert_no_cycles_present()
 
-    def test_does_not_fire_exception_if_no_cycles_present(self):
-        graph_with_no_cycles = {}
-        graph_with_no_cycles['foo'] = ['bar']
-        graph_with_no_cycles['a'] = ['b']
-        graph_with_no_cycles['baz'] = ['bar']
-        graph_with_no_cycles['hello'] = ['foo']
-        graphTest = TokenCycleChecking(graph_with_no_cycles)
-        graphTest.assert_no_cycles_present()
+        actual_graph = TokenCycleChecking(empty_graph)
 
-    def test_recognizes_complex_cycle(self):
-        graph_with_cycle = {}
-        graph_with_cycle['foo'] = ['bar']
-        graph_with_cycle['bar'] = ['baz']
-        graph_with_cycle['baz'] = ['abc']
-        graph_with_cycle['abc'] = ['foo']
-        graph_with_cycle['f'] = ['g']
-        graph_with_cycle['j'] = ['k']
-        graphTest = TokenCycleChecking(graph_with_cycle)
-        self.assertRaises(ContainsCyclesException, graphTest.assert_no_cycles_present)
+        actual_graph.assert_no_cycles_present()
+
+    def test_should_not_raise_exception_if_no_cycles_present(self):
+        graph_with_no_cycles = {'foo': ['bar'],
+                                'a': ['b'],
+                                'baz': ['bar'],
+                                'hello': ['foo']}
+
+        actual_graph = TokenCycleChecking(graph_with_no_cycles)
+
+        actual_graph.assert_no_cycles_present()
+
+    def test_should_recognize_complex_cycle(self):
+        graph_with_cycle = {'foo': ['bar'],
+                            'bar': ['baz'],
+                            'baz': ['abc'],
+                            'abc': ['foo'],
+                            'f': ['g'],
+                            'j': ['k']}
+
+        actual_graph = TokenCycleChecking(graph_with_cycle)
+
+        self.assertRaises(ContainsCyclesException, actual_graph.assert_no_cycles_present)
