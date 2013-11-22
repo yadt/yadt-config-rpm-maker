725612be9668564cdcbf6fb312be53d23f869da3
aelgru <aelgru@gmail.com>
2013-11-22 18:38:35 +0100

    Adding should to test name.

--- test/integrationtests/hostresolver_integration_test.py
+++ test/integrationtests/hostresolver_integration_test.py
@@ -20,7 +20,10 @@ from config_rpm_maker.hostResolver import HostResolver
 
 
 class HostResolverTest(unittest.TestCase):
-    def test_resolve_localhost(self):
+
+    def test_should_resolve_localhost(self):
+
         ip, fqdn, aliases = HostResolver().resolve('localhost')
+
         self.assertTrue(ip == '127.0.0.1' or ip == '::1')
         self.assertEqual(fqdn, 'localhost')
