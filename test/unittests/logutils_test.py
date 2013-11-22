98566cd9cb8448b7655e435d4fb600975f01e9a8
aelgru <aelgru@gmail.com>
2013-11-22 11:27:10 +0100

    Adding test coverage for create_sys_log_handler.

--- test/unittests/logutils_test.py
+++ test/unittests/logutils_test.py
@@ -17,11 +17,12 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from logging import ERROR
-from unittest import TestCase
 from mock import Mock, patch
+from unittest import TestCase
 
-from config_rpm_maker.config import DEFAULT_LOG_LEVEL
-from config_rpm_maker.logutils import create_console_handler
+from config_rpm_maker.config import DEFAULT_LOG_LEVEL, DEFAULT_SYS_LOG_LEVEL
+from config_rpm_maker.logutils import create_console_handler, create_sys_log_handler
+
 
 @patch('config_rpm_maker.logutils.StreamHandler')
 @patch('config_rpm_maker.logutils.Formatter')
@@ -63,5 +64,40 @@ class CreateConsoleHandlerTests(TestCase):
         mock_stream_handler_class.return_value = mock_handler
 
         actual_handler = create_console_handler()
+
+        self.assertEqual(mock_handler, actual_handler)
+
+
+@patch('config_rpm_maker.logutils.SysLogHandler')
+@patch('config_rpm_maker.logutils.Formatter')
+class CreateSysLogHandlerTests(TestCase):
+
+    def test_should_initialze_formatter_using_the_revision_number_in_the_format(self, mock_formatter_class, mock_sys_log_handler_class):
+
+        mock_formatter = Mock()
+        mock_formatter_class.return_value = mock_formatter
+        mock_handler = Mock()
+        mock_sys_log_handler_class.return_value = mock_handler
+
+        create_sys_log_handler(123)
+
+        mock_formatter_class.assert_called_with('config_rpm_maker[123]: [%(levelname)5s] %(message)s')
+        mock_handler.setFormatter.assert_called_with(mock_formatter)
+
+    def test_should_set_default_log_level_if_no_log_level_given(self, mock_formatter_class, mock_sys_log_handler_class):
+
+        mock_handler = Mock()
+        mock_sys_log_handler_class.return_value = mock_handler
+
+        create_sys_log_handler(123)
+
+        mock_handler.setLevel.assert_called_with(DEFAULT_SYS_LOG_LEVEL)
+
+    def test_should_return_created_console_handler(self, mock_formatter_class, mock_sys_log_handler_class):
+
+        mock_handler = Mock()
+        mock_sys_log_handler_class.return_value = mock_handler
+
+        actual_handler = create_sys_log_handler(123)
 
         self.assertEqual(mock_handler, actual_handler)
