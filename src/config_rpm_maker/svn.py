d845d368b38c300533465f074ad1f503fe6757fa
aelgru <aelgru@gmail.com>
2013-11-22 16:27:14 +0100

    Refactoring: extracted variable repos_paths.

--- src/config_rpm_maker/svn.py
+++ src/config_rpm_maker/svn.py
@@ -68,7 +68,8 @@ class SvnService(object):
 
         # remove first item
         items = items[1:]
-        return [os.path.basename(item[0].repos_path) for item in items]
+        repos_paths = [item[0].repos_path for item in items]
+        return [os.path.basename(repos_path) for repos_path in repos_paths]
 
     def export(self, svn_path, target_dir, revision):
         url = self._get_url(svn_path)
