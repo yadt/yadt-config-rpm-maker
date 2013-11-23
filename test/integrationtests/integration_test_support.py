eb3fd6553e63032ded10c0991926eb0853606528
aelgru <aelgru@gmail.com>
2013-11-23 15:31:03 +0100

    Created class IntegrationTestException - this helps the developer to understand that the test setup itself failed.

--- test/integrationtests/integration_test_support.py
+++ test/integrationtests/integration_test_support.py
@@ -22,6 +22,10 @@ import unittest
 from config_rpm_maker import config
 
 
+class IntegrationTestException(Exception):
+    pass
+
+
 class IntegrationTest(unittest.TestCase):
 
     def create_svn_repo(self):
@@ -29,10 +33,14 @@ class IntegrationTest(unittest.TestCase):
         if os.path.exists(self.repo_dir):
             shutil.rmtree(self.repo_dir)
         os.makedirs(self.repo_dir)
+
         if subprocess.call('svnadmin create %s' % self.repo_dir, shell=True):
-            raise Exception('Could not create svn repo.')
+            raise IntegrationTestException('Could not create svn repo.')
+
         self.repo_url = 'file://%s' % self.repo_dir
+
         if subprocess.call('svn import -q -m import testdata/svn_repo %s' % self.repo_url, shell=True):
-            raise Exception('Could not import test data.')
+            raise IntegrationTestException('Could not import test data.')
+
         if subprocess.call('svn import -q -m import testdata/index.html %s/config/typ/web/data/index.html' % self.repo_url, shell=True):
-            raise Exception('Could not import test data.')
+            raise IntegrationTestException('Could not import test data.')
