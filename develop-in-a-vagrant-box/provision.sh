f42ef5e83c67067f3faebfb7bcaa0ada596898c8
aelgru <aelgru@gmail.com>
2013-12-11 18:23:59 +0100

    More feedback after provisioning. Closes #26

--- develop-in-a-vagrant-box/provision.sh
+++ develop-in-a-vagrant-box/provision.sh
@@ -22,6 +22,7 @@ source "configuration.sh"
 source "library.sh"
 
 if [[ -e ${ALREADY_PROVISIONED} ]]; then
+  echo ""
   echo "This VM has already been provisioned successfully."
   echo "Exiting without doing anything ..."
   exit 0
@@ -31,6 +32,10 @@ install_dependencies
 build_and_install_config_rpm_maker
 setup_svn_server_with_test_data_and_start_it
 
+echo ""
 echo "Touching file ${ALREADY_PROVISIONED}"
 echo "to mark this VM as already provisioned."
 touch ${ALREADY_PROVISIONED}
+
+echo ""
+echo "Provisioning was successful."
