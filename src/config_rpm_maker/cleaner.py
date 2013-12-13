a1d4651065f31c6170d63d2e7e0b05b679d8a4ef
aelgru <aelgru@gmail.com>
2013-12-13 18:12:14 +0100

    Extracting function _delete_host_directories

--- src/config_rpm_maker/cleaner.py
+++ src/config_rpm_maker/cleaner.py
@@ -36,30 +36,38 @@ def clean_up_deleted_hosts_data(svn_service, revision):
 
     if deleted_paths:
         LOGGER.debug("Change set contains %d deleted path(s).", len(deleted_paths))
-        svn_prefix = Host().get_svn_prefix()
-        svn_prefix_length = len(svn_prefix)
-
-        for deleted_path in deleted_paths:
-            if deleted_path.startswith(svn_prefix):
-                host_name = deleted_path[svn_prefix_length:]
-                if _is_a_host_name_and_not_a_path(host_name):
-                    _delete_host_directory(host_name)
+        _delete_host_directories(deleted_paths)
     else:
         verbose(LOGGER).debug("Change set did not contain any deleted paths.")
 
 
-def _delete_host_directory(host_name):
-    """ deletes the config viewer data for the given host name """
-    
-    host_directory = build_config_viewer_host_directory(host_name)
-    if exists(host_directory):
-        LOGGER.info('Deleting config viewer data for host "%s"', host_name)
-        rmtree(host_directory)
-    else:
-        verbose(LOGGER).debug('Wanted to delete host directory "%s", but it did not exist.' % host_directory)
+def _delete_host_directories(deleted_paths):
+    """ checks for each given path if it contains the svn_prefix for a host
+        and if it does it will check if the rest of the path is a host name
+        if so it will delete the corresponding directory"""
+
+    svn_prefix = Host().get_svn_prefix()
+    svn_prefix_length = len(svn_prefix)
+
+    for deleted_path in deleted_paths:
+        if deleted_path.startswith(svn_prefix):
+            host_name = deleted_path[svn_prefix_length:]
+            if _is_a_host_name_and_not_a_path(host_name):
+                _delete_host_directory(host_name)
 
 
 def _is_a_host_name_and_not_a_path(host_name):
     """ checks if the given string contains a path separator character """
 
     return host_name.find(PATH_SEPARATOR) == -1
+
+
+def _delete_host_directory(host_name):
+    """ deletes the config viewer data for the given host name """
+
+    host_directory = build_config_viewer_host_directory(host_name)
+    if exists(host_directory):
+        LOGGER.info('Deleting config viewer data for host "%s"', host_name)
+        rmtree(host_directory)
+    else:
+        verbose(LOGGER).debug('Wanted to delete host directory "%s", but it did not exist.' % host_directory)
