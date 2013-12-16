bc6a5d1641d4d5197ff4d1c8cfc8c39ae0fa0a04
aelgru <aelgru@gmail.com>
2013-12-16 23:40:40 +0100

    Deleting useless constant DEFAULT_UPLOAD_CHUNK_SIZE (since there is anotherone describing the same) and sorting properties.

--- src/config_rpm_maker/configuration/properties.py
+++ src/config_rpm_maker/configuration/properties.py
@@ -14,48 +14,69 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-DEFAULT_ALLOW_UNKNOWN_HOSTS = True
-DEFAULT_CONFIG_VIEWER_DIR = '/tmp'
-DEFAULT_CONFIG_VIEWER_ONLY = False
-DEFAULT_CONFIG_RPM_PREFIX = 'yadt-config-'
-DEFAULT_CUSTOM_DNS_SEARCHLIST = []
-DEFAULT_ERROR_LOG_URL = ''
-DEFAULT_ERROR_LOG_DIRECTORY = ""
-DEFAULT_MAX_FAILED_HOSTS = 3
-DEFAULT_MAX_FILE_SIZE = 100 * 1024
-DEFAULT_HOST_NAME_ENCODING = 'ascii'
-DEFAULT_LOG_FORMAT = "[%(levelname)5s] %(message)s"
-DEFAULT_LOG_LEVEL = 'DEBUG'
-DEFAULT_NO_CLEAN_UP = False
-DEFAULT_PATH_TO_SPEC_FILE = 'default.spec'
-DEFAULT_REPO_PACKAGES_REGEX = '.*-repo.*'
-DEFAULT_RPM_UPLOAD_CHUNK_SIZE = 10
-DEFAULT_RPM_UPLOAD_COMMAND = None
-DEFAULT_SVN_PATH_TO_CONFIG = '/config'
-DEFAULT_THREAD_COUNT = 1
-DEFAULT_TEMP_DIR = '/tmp'
-DEFAULT_UPLOAD_CHUNK_SIZE = 0
-DEFAULT_VERBOSE = False
 
+
+DEFAULT_ALLOW_UNKNOWN_HOSTS = True
 KEY_ALLOW_UNKNOWN_HOSTS = 'allow_unknown_hosts'
-KEY_CONFIG_VIEWER_ONLY = 'config_viewer_only'
+
 KEY_CONFIG_VIEWER_HOSTS_DIR = 'config_viewer_hosts_dir'
+DEFAULT_CONFIG_VIEWER_DIR = '/tmp'
+
+DEFAULT_CONFIG_VIEWER_ONLY = False
+KEY_CONFIG_VIEWER_ONLY = 'config_viewer_only'
+
+DEFAULT_CONFIG_RPM_PREFIX = 'yadt-config-'
 KEY_CONFIG_RPM_PREFIX = 'config_rpm_prefix'
+
+DEFAULT_CUSTOM_DNS_SEARCHLIST = []
 KEY_CUSTOM_DNS_SEARCHLIST = 'custom_dns_searchlist'
-KEY_ERROR_LOG_DIRECTORY = 'error_log_dir'
+
+DEFAULT_ERROR_LOG_URL = ''
 KEY_ERROR_LOG_URL = 'error_log_url'
-KEY_LOG_FORMAT = "log_format"
-KEY_LOG_LEVEL = "log_level"
-KEY_MAX_FILE_SIZE = 'max_file_size'
+
+DEFAULT_ERROR_LOG_DIRECTORY = ""
+KEY_ERROR_LOG_DIRECTORY = 'error_log_dir'
+
+DEFAULT_MAX_FAILED_HOSTS = 3
 KEY_MAX_FAILED_HOSTS = 'max_failed_hosts'
+
+DEFAULT_MAX_FILE_SIZE = 100 * 1024
+KEY_MAX_FILE_SIZE = 'max_file_size'
+
+DEFAULT_HOST_NAME_ENCODING = 'ascii'
+
+DEFAULT_LOG_FORMAT = "[%(levelname)5s] %(message)s"
+KEY_LOG_FORMAT = "log_format"
+
+DEFAULT_LOG_LEVEL = 'DEBUG'
+KEY_LOG_LEVEL = "log_level"
+
+DEFAULT_NO_CLEAN_UP = False
 KEY_NO_CLEAN_UP = 'no_clean_up'
+
+DEFAULT_PATH_TO_SPEC_FILE = 'default.spec'
 KEY_PATH_TO_SPEC_FILE = 'path_to_spec_file'
-KEY_RPM_UPLOAD_CHUNK_SIZE = 'rpm_upload_chunk_size'
-KEY_RPM_UPLOAD_COMMAND = 'rpm_upload_cmd'
+
+DEFAULT_REPO_PACKAGES_REGEX = '.*-repo.*'
 KEY_REPO_PACKAGES_REGEX = 'repo_packages_regex'
+
+DEFAULT_RPM_UPLOAD_CHUNK_SIZE = 10
+KEY_RPM_UPLOAD_CHUNK_SIZE = 'rpm_upload_chunk_size'
+
+DEFAULT_RPM_UPLOAD_COMMAND = None
+KEY_RPM_UPLOAD_COMMAND = 'rpm_upload_cmd'
+
+DEFAULT_SVN_PATH_TO_CONFIG = '/config'
 KEY_SVN_PATH_TO_CONFIG = 'svn_path_to_config'
+
+DEFAULT_THREAD_COUNT = 1
+KEY_THREAD_COUNT = 'thread_count'
+
+DEFAULT_TEMP_DIR = '/tmp'
+KEY_TEMP_DIR = 'temp_dir'
+
+DEFAULT_VERBOSE = False
+KEY_VERBOSE = 'verbose'
+
 KEY_SVN_PATH_TO_CONFIGURATION = 'svn_path_to_config'
 KEY_TEMPORARY_DIRECTORY = "temp_dir"
-KEY_THREAD_COUNT = 'thread_count'
-KEY_TEMP_DIR = 'temp_dir'
-KEY_VERBOSE = 'verbose'
