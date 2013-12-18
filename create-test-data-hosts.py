8271c78bdba7ee8d74b375c3dae1ad445612065f
aelgru <aelgru@gmail.com>
2013-12-18 17:56:36 +0100

    Grrr ... fixing names ... these are not types, but locations.

--- create-test-data-hosts.py
+++ create-test-data-hosts.py
@@ -5,60 +5,60 @@ from shutil import copytree
 from random import choice, randint
 from string import ascii_letters
 
-MAX_TYPE1_HOSTS = 5
-MAX_TYPE2_HOSTS = 5
-MAX_TYPE3_HOSTS = 20
+MAX_LOCATION1_HOSTS = 5
+MAX_LOCATION2_HOSTS = 5
+MAX_LOCATION3_HOSTS = 20
 
-MIN_TYPE1_HOSTS = 3
-MIN_TYPE2_HOSTS = 1
-MIN_TYPE3_HOSTS = 5
+MIN_LOCATION1_HOSTS = 3
+MIN_LOCATION2_HOSTS = 1
+MIN_LOCATION3_HOSTS = 5
 
 CONFIGURATION_DIRECTORY = 'testdata/svn_repo/config'
 
-BASE_TYPE1_HOST = '{configuration_directory}/host/devweb01'.format(configuration_directory=CONFIGURATION_DIRECTORY)
-BASE_TYPE2_HOST = '{configuration_directory}/host/tuvweb01'.format(configuration_directory=CONFIGURATION_DIRECTORY)
-BASE_TYPE3_HOST = '{configuration_directory}/host/berweb01'.format(configuration_directory=CONFIGURATION_DIRECTORY)
+BASE_LOCATION1_HOST = '{configuration_directory}/host/devweb01'.format(configuration_directory=CONFIGURATION_DIRECTORY)
+BASE_LOCATION2_HOST = '{configuration_directory}/host/tuvweb01'.format(configuration_directory=CONFIGURATION_DIRECTORY)
+BASE_LOCATION3_HOST = '{configuration_directory}/host/berweb01'.format(configuration_directory=CONFIGURATION_DIRECTORY)
 
-TYPE1_HOST_DESTINATION = '{configuration_directory}/host/dev{abbreviation}%02d'
-TYPE2_HOST_DESTINATION = '{configuration_directory}/host/tuv{abbreviation}%02d'
-TYPE3_HOST_DESTINATION = '{configuration_directory}/host/ber{abbreviation}%02d'
+LOCATION1_HOST_DESTINATION = '{configuration_directory}/host/dev{abbreviation}%02d'
+LOCATION2_HOST_DESTINATION = '{configuration_directory}/host/tuv{abbreviation}%02d'
+LOCATION3_HOST_DESTINATION = '{configuration_directory}/host/ber{abbreviation}%02d'
 
 
 def create_type1_host(abbreviation, host_number):
-    dev_format = TYPE1_HOST_DESTINATION.format(configuration_directory=CONFIGURATION_DIRECTORY,
-                                               abbreviation=abbreviation)
+    dev_format = LOCATION1_HOST_DESTINATION.format(configuration_directory=CONFIGURATION_DIRECTORY,
+                                                   abbreviation=abbreviation)
     host_dir = dev_format % host_number
     if not exists(host_dir):
-        copytree(BASE_TYPE1_HOST, host_dir)
+        copytree(BASE_LOCATION1_HOST, host_dir)
 
 
 def create_type2_host(abbreviation, host_number):
-    tuv_format = TYPE2_HOST_DESTINATION.format(configuration_directory=CONFIGURATION_DIRECTORY,
-                                               abbreviation=abbreviation)
+    tuv_format = LOCATION2_HOST_DESTINATION.format(configuration_directory=CONFIGURATION_DIRECTORY,
+                                                   abbreviation=abbreviation)
     host_dir = tuv_format % host_number
     if not exists(host_dir):
-        copytree(BASE_TYPE2_HOST, host_dir)
+        copytree(BASE_LOCATION2_HOST, host_dir)
 
 
 def create_type3_host(abbreviation, host_number):
-    prod_format = TYPE3_HOST_DESTINATION.format(configuration_directory=CONFIGURATION_DIRECTORY,
-                                                abbreviation=abbreviation)
+    prod_format = LOCATION3_HOST_DESTINATION.format(configuration_directory=CONFIGURATION_DIRECTORY,
+                                                    abbreviation=abbreviation)
     host_dir = prod_format % host_number
     if not exists(host_dir):
-        copytree(BASE_TYPE3_HOST, host_dir)
+        copytree(BASE_LOCATION3_HOST, host_dir)
 
 
 def create_type(abbreviation):
 
-    count_of_development_hosts = randint(MIN_TYPE1_HOSTS, MAX_TYPE1_HOSTS)
+    count_of_development_hosts = randint(MIN_LOCATION1_HOSTS, MAX_LOCATION1_HOSTS)
     for host_number in range(1, count_of_development_hosts):
         create_type1_host(abbreviation, host_number)
 
-    count_of_test_hosts = randint(MIN_TYPE2_HOSTS, MAX_TYPE2_HOSTS)
+    count_of_test_hosts = randint(MIN_LOCATION2_HOSTS, MAX_LOCATION2_HOSTS)
     for host_number in range(1, count_of_test_hosts):
         create_type2_host(abbreviation, host_number)
 
-    count_of_production_hosts = randint(MIN_TYPE3_HOSTS, MAX_TYPE3_HOSTS)
+    count_of_production_hosts = randint(MIN_LOCATION3_HOSTS, MAX_LOCATION3_HOSTS)
     for host_number in range(1, count_of_production_hosts):
         create_type3_host(abbreviation, host_number)
 
