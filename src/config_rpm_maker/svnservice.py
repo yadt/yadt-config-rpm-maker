751af2e45cb1dc2b017d2db6960342040b883419
mriehl <max@riehl.io>
2015-02-13 09:25:27 +0100

    reflow comment for readability

--- src/config_rpm_maker/svnservice.py
+++ src/config_rpm_maker/svnservice.py
@@ -1,5 +1,5 @@
 # yadt-config-rpm-maker
-#   Copyright (C) 2011-2013 Immobilien Scout GmbH
+# Copyright (C) 2011-2013 Immobilien Scout GmbH
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
